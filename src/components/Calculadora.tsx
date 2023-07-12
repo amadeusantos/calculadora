c3094e2afa6a6f93b26f8298745ecbcf81044874
Amadeu Santos <amadeusantos31@gmail.com>
2023-07-12 18:52:45 -0300

    montando a calculadora

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -1,7 +1,14 @@
+import Button from "./Button";
+import Display from "./Display";
+
 export default function Calculadora() {
+    const teclas = ["1", "2", "3", "MC", "MR", "+", "4", "5", "6", "M+", "M-", "-", "7", "8", "9", "*", "/", "DEL", ".", "0", "_", "_", "%", "="]
     return (
-        <div>
-            
+        <div className="w-screen h-screen bg-black">
+            <Display valor="10" />
+            <div className="h-3/4">
+                {teclas.map((tecla) => <Button key={tecla} valor={tecla} />)}
+            </div>
         </div>
     )
 }
