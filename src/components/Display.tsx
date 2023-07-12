e1b765adffd6bcd87acc3c617d0601c843232f34
Amadeu Santos <amadeusantos31@gmail.com>
2023-07-12 18:26:05 -0300

    fazendo o estilo do Display

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,7 +1,11 @@
-export default function Display() {
+interface DisplayProps {
+    valor: string
+}
+
+export default function Display({valor}: DisplayProps) {
     return (
-        <div>
-            
+        <div className="flex justify-end items-center w-screen h-1/4 p-12 text-9xl bg-white text-cyan-600">
+            {valor}
         </div>
     )
 }
