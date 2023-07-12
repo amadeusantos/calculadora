9a85b98402bd35ff858ddb06d67704ff9bfa6e6c
Amadeu Santos <amadeusantos31@gmail.com>
2023-07-12 18:20:59 -0300

    fazendo o estilo do Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
-export default function Button() {
+interface ButtonsProps {
+    valor: string
+}
+
+export default function Button({valor}: ButtonsProps) {
     return (
-        <button>
-            
+        <button className="w-1/6 h-1/5 text-5xl border bg-slate-900 border-slate-600">
+            {valor}
         </button>
     )
 }
